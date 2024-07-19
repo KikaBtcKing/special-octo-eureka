a02ca757e8b92a323d4d15fde275c65352a2200a
KikaBtcKing <Goldendragonlogistics@gmail.com>
2024-07-19 18:15:30 +0900

    Add files via upload

---
+++ ДЗ№2.py
@@ -0,0 +1,6 @@
+print (9**0.5*5)  # "1st program"
+print (9.99 > 9.98 and 1000 != 1000.1)  # "2nd program"
+print (1234 // 10 % 100)  # "3rd program"
+print (5678 // 10 % 100)
+print (23 + 67)
+print (13.42 / 10 % 100)   # "4th program"
