e062916ee1f586e836c495a603c0fe5486bbc706
KikaBtcKing <Goldendragonlogistics@gmail.com>
2024-07-23 14:39:04 +0900

    Add files via upload

---
+++ HomeWork6.py
@@ -0,0 +1,24 @@
+  # mutable list = ["Audi", "BMW", 100, "Mercedes-Benz", True]
+  # print (mutable list)
+  # print (type (mutable list))
+  # mutable list .append ("Toyota")
+  # print (mutable list)
+  # mutable list .remove (100)
+  # print (mutable list)
+  # mutable list .extend ("GAZ")
+  # print (mutable list)
+  # print (True in mutable list)
+  # print ("Mercedes-Benz"  not in mutable list)
+  # mutable list[3] = "Maserati"
+  # print (mutable list)
+
+immutable_list = 150, 500, False, "Barrakuda"
+print (immutable_list)
+print (type (immutable_list))
+immutable_list[0] = 200
+print (immutable_list)
+immutable_list .append [100]
+print (immutable_list)
+  # Кортеж - это неизменяемая база данных, создана с целью сохранения данных в их первозданном виде.
+  # Плюс ко всему занимает меньше свободного места, чем список!
+
