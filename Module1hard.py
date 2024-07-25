e03ede88a2e9db05e51143ddf2d0699080684a28
KikaBtcKing <Goldendragonlogistics@gmail.com>
2024-07-25 11:12:17 +0900

    Add files via upload

---
+++ Module1hard.py
@@ -0,0 +1,13 @@
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+print(grades)
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(students)
+print(type(grades))
+print(type(students))
+grades =((5 + 3 + 3 + 5 + 4) / 5), ((2 + 2 + 2 + 3) / 4), ((4 + 5 + 5 + 2) / 4), ((4 + 4 + 3 )/ 3), ((5 + 5 + 5 + 4 + 5) / 5)
+print(grades)
+class_11A = {"Aaron": 4.0, "Bilbo": 2.25, "Johny": 4.0, "Khendrick": 3.6666666666666665, "Steve": 4.8}
+print(class_11A)
+print(type(class_11A))
+print(class_11A["Aaron"])
